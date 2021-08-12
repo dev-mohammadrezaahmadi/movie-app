49f180f86095fc655b006eb2e40367c69184f963
dev-mohammadrezaahmadi <dev.mohammadrezaahmadi@gmail.com>
2021-08-13 04:21:19 +0430

    change routes naming

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
-import type { NextPage } from "next";
+import type { NextPage, GetServerSideProps } from "next";
 import Head from "next/head";
 import Header from "../components/Header";
 import Nav from "../components/Nav";
+import MoviesList from "../components/MoviesList";
+import requests from "../utils/requests";
 
-const Home: NextPage = () => {
+interface HomePagePorps {
+	results: any[];
+}
+
+const Home: NextPage<HomePagePorps> = ({ results }) => {
 	return (
 		<div>
 			<Head>
@@ -14,8 +20,23 @@ const Home: NextPage = () => {
 
 			<Header />
 			<Nav />
+			<MoviesList results={results} />
 		</div>
 	);
 };
 
 export default Home;
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+	const genre = ctx.query.genre;
+	const request = await fetch(
+		`https://api.themoviedb.org/3${
+			requests[genre as string]?.url || requests["trending"].url
+		}`
+	).then((res) => res.json());
+	return {
+		props: {
+			results: request?.results,
+		},
+	};
+};
--- utils/requests.ts
+++ utils/requests.ts
@@ -8,49 +8,49 @@ interface IFetchRequests {
 }
 
 const REQUESTS: IFetchRequests = {
-	fetchTrending: {
+	trending: {
 		title: "Trending",
 		url: `/trending/all/week?api_key=${API_KEY}&language=en-us`,
 	},
-	fetchTopRated: {
+	"top rated": {
 		title: "Top Rated",
 		url: `/movie/top_rated?api_key=${API_KEY}&language=en-us`,
 	},
-	fetchActionMovies: {
+	action: {
 		title: "Action",
 		url: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
 	},
-	fetchComedyMovies: {
+	comedy: {
 		title: "Comedy",
-		url: `/dicover/movie?api_key=${API_KEY}&with_genres=35`,
+		url: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
 	},
-	fetchHorrorMovies: {
+	horror: {
 		title: "Horror",
-		url: `/dicover/movie?api_key=${API_KEY}&with_genres=27`,
+		url: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
 	},
-	fetchRomanceMovies: {
+	romance: {
 		title: "Romance",
-		url: `/dicover/movie?api_key=${API_KEY}&with_genres=10749`,
+		url: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
 	},
-	fetchMysteryMovies: {
+	mystery: {
 		title: "Mystery",
-		url: `/dicover/movie?api_key=${API_KEY}&with_genres=9648`,
+		url: `/discover/movie?api_key=${API_KEY}&with_genres=9648`,
 	},
-	fetchSciFiMovies: {
+	"sci-fi": {
 		title: "Sci-Fi",
-		url: `/dicover/movie?api_key=${API_KEY}&with_genres=878`,
+		url: `/discover/movie?api_key=${API_KEY}&with_genres=878`,
 	},
-	fetchWesternMovies: {
+	western: {
 		title: "Western",
-		url: `/dicover/movie?api_key=${API_KEY}&with_genres=37`,
+		url: `/discover/movie?api_key=${API_KEY}&with_genres=37`,
 	},
-	fetchAnimationMovies: {
+	animation: {
 		title: "Animation",
-		url: `/dicover/movie?api_key=${API_KEY}&with_genres=16`,
+		url: `/discover/movie?api_key=${API_KEY}&with_genres=16`,
 	},
-	fetchTVShows: {
-		title: "TV Shows",
-		url: `/dicover/movie?api_key=${API_KEY}&with_genres=10770`,
+	"tv-show": {
+		title: "TV-Show",
+		url: `/discover/movie?api_key=${API_KEY}&with_genres=10770`,
 	},
 };
 
