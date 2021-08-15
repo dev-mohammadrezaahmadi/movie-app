b67f0e8cc94d3442bbb96f57fd1cc8444a47b52a
dev-mohammadrezaahmadi <dev.mohammadrezaahmadi@gmail.com>
2021-08-15 20:37:18 +0430

    clean up redundant codes

--- components/MoviesList.tsx
+++ components/MoviesList.tsx
@@ -5,9 +5,6 @@ interface MoviesListProps {
 }
 
 const MoviesList: React.FC<MoviesListProps> = ({ results }) => {
-	// __dev__
-	// console.log(results)
-
 	return (
 		<div className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
 			{results.map((result) => (
--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import { ThumbUpIcon } from "@heroicons/react/outline";
 
 interface ThumnailProps {
@@ -8,35 +9,37 @@ interface ThumnailProps {
 const Thumbnail: React.FC<ThumnailProps> = ({ result }) => {
 	const BASE_URL = "https://image.tmdb.org/t/p/original/";
 	return (
-		<div className="group cursor-pointer p-2 transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
-			<Image
-				layout="responsive"
-				height={1080}
-				width={1920}
-				alt="movie poste thumbnail"
-				src={
-					`${BASE_URL}${result.backdrop_path || result.poster_path}` ||
-					`${BASE_URL}${result.poster_path}`
-				}
-				blurDataURL={
-					`${BASE_URL}${result.backdrop_path || result.poster_path}` ||
-					`${BASE_URL}${result.poster_path}`
-				}
-				loading="lazy"
-				placeholder="blur"
-			/>
-			<div className="p-2">
-				{/* <p className="truncate max-w-md ">{result.overview}</p> */}
-				<h2 className="mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold">
-					{result.title || result.original_name}
-				</h2>
-				<p className="flex items-center opacity-0 group-hover:opacity-100">
-					{result.media_type && `${result.media_type}`}{" "}
-					{result.release_date || result.first_air_date}{" "}
-					<ThumbUpIcon className="h-5 mx-2" /> {result.vote_count}
-				</p>
+		<Link href={`/${result.id}`} passHref>
+			<div className="group cursor-pointer p-2 transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
+				<Image
+					layout="responsive"
+					height={1080}
+					width={1920}
+					alt="movie poste thumbnail"
+					src={
+						`${BASE_URL}${result.backdrop_path || result.poster_path}` ||
+						`${BASE_URL}${result.poster_path}`
+					}
+					blurDataURL={
+						`${BASE_URL}${result.backdrop_path || result.poster_path}` ||
+						`${BASE_URL}${result.poster_path}`
+					}
+					loading="lazy"
+					placeholder="blur"
+				/>
+				<div className="p-2">
+					{/* <p className="truncate max-w-md ">{result.overview}</p> */}
+					<h2 className="mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold">
+						{result.title || result.original_name}
+					</h2>
+					<p className="flex items-center opacity-0 group-hover:opacity-100">
+						{result.media_type && `${result.media_type}`}{" "}
+						{result.release_date || result.first_air_date}{" "}
+						<ThumbUpIcon className="h-5 mx-2" /> {result.vote_count}
+					</p>
+				</div>
 			</div>
-		</div>
+		</Link>
 	);
 };
 
